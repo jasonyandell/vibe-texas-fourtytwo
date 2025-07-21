a737c19a14063cab69cd1fd141dfd77801280dbe
Jason Yandell <jasonyandell@gmail.com>
2025-07-21 01:26:48 -0500

    fixing tests

--- frontend/src/components/__tests__/DominoComponent.test.tsx
+++ frontend/src/components/__tests__/DominoComponent.test.tsx
@@ -1,13 +1,6 @@
-import { describe } from 'vitest'
-
 // Import all test modules
 import './DominoComponent.rendering.test'
 import './DominoComponent.interactive.test'
 import './DominoComponent.states.test'
 import './DominoComponent.combinations.test'
 import './DominoComponent.accessibility.test'
-
-describe('DominoComponent Test Suite', () => {
-  // This file aggregates all DominoComponent tests
-  // Individual test modules are imported above
-})
